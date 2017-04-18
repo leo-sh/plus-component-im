9bf21fe63ba890a0e1594aa244428061eafc72c8
Seven <shiweidu@outlook.com>
2017-04-18 14:33:25 +0800

    Fixed db table error.

--- src/Database/im_conversations_table.php
+++ src/Database/im_conversations_table.php
@@ -7,13 +7,13 @@ if (!Schema::hasTable('im_conversations', 'data')) {
     Schema::create('im_conversations', function (Blueprint $table) {
         $table->engine = 'InnoDB';
         $table->increments('id')->comment('对话表表ID');
-        $table->integer('user_id')->default(0)->comment('创建对话用户UID');
-        $table->bigInteger('cid')->index()->default(0)->comment('对话id');
-        $table->string('name')->dafault(null)->comment('对话名称');
-        $table->string('pwd')->default(null)->comment('加入对话密码');
-        $table->tinyInteger('is_disabled')->default(0)->comment('是否被禁用,1:是 0:否');
-        $table->tinyInteger('type')->default(0)->comment('对话类型 0:私聊 1:群聊 2:聊天室');
-        $table->text('uids')->default(null)->comment('已加入聊天的用户UID');
+        $table->integer('user_id')->nullable()->default(0)->comment('创建对话用户UID');
+        $table->bigInteger('cid')->index()->nullable()->default(0)->comment('对话id');
+        $table->string('name')->nullable()->dafault('')->comment('对话名称');
+        $table->string('pwd')->nullable()->default('')->comment('加入对话密码');
+        $table->tinyInteger('is_disabled')->nullable()->default(0)->comment('是否被禁用,1:是 0:否');
+        $table->tinyInteger('type')->nullable()->default(0)->comment('对话类型 0:私聊 1:群聊 2:聊天室');
+        $table->text('uids')->nullable()->default('')->comment('已加入聊天的用户UID');
         $table->timestamps();
     });
 }
--- src/Database/im_users_table.php
+++ src/Database/im_users_table.php
@@ -7,9 +7,9 @@ if (!Schema::hasTable('im_users', 'data')) {
     Schema::create('im_users', function (Blueprint $table) {
         $table->engine = 'InnoDB';
         $table->increments('id')->comment('表ID');
-        $table->integer('user_id')->unique()->default(0)->comment('用户ID');
-        $table->string('im_password')->default(null)->comment('聊天用户登陆的密码');
-        $table->tinyInteger('is_disabled')->default(0)->comment('是否被禁用,1:是 0:否');
+        $table->integer('user_id')->unique()->nullable()->default(0)->comment('用户ID');
+        $table->string('im_password')->nullable()->default('')->comment('聊天用户登陆的密码');
+        $table->tinyInteger('is_disabled')->nullable()->default(0)->comment('是否被禁用,1:是 0:否');
         $table->timestamps();
         $table->softDeletes();
     });
