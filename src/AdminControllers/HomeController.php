441005968f56d577ac61578ade8819c44249c677
Seven <shiweidu@outlook.com>
2017-04-18 17:40:30 +0800

    add base view.

--- src/AdminControllers/HomeController.php
+++ src/AdminControllers/HomeController.php
@@ -2,12 +2,24 @@
 
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentIm\AdminControllers;
 
+use Illuminate\Http\Request;
 use Zhiyi\Plus\Http\Controllers\Controller;
+use function Zhiyi\Component\ZhiyiPlus\PlusComponentIm\view;
 
 class HomeController extends Controller
 {
-    public function show()
+    /**
+     * The component manage entry.
+     *
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function show(Request $request)
     {
-        var_dump('hello.');
+        if (! $request->user()) {
+            return redirect(route('admin'), 302);
+        }
+        
+        return view('admin');
     }
 }
---
+++ views/admin.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>即时通讯 - ThinkSNS+</title>
+    <style type="text/css">@include('admin.style')</style>
+</head>
+<body>
+    <div></div>
+</body>
+</html>
