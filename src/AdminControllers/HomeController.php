0956026d0d21624e64cacdc945ae744d2adc1b45
Seven <shiweidu@outlook.com>
2017-04-18 16:38:34 +0800

    add admin manage entry controller.

---
+++ src/AdminControllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zhiyi\Component\ZhiyiPlus\PlusComponentIm\AdminControllers;
+
+use Zhiyi\Plus\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function show()
+    {
+        var_dump('hello.');
+    }
+}
