0de82a606b71652d2aedf32d693d3b82456fbf7b
Seven <shiweidu@outlook.com>
2017-04-18 17:27:05 +0800

    add view function && 优化 base_path.

--- src/helpers.php
+++ src/helpers.php
@@ -43,6 +43,10 @@ function asset_path($path)
  */
 function base_path($path = '')
 {
+    if (! $path) {
+        return dirname(__DIR__);
+    }
+
     return dirname(__DIR__).'/'.$path;
 }
 
@@ -81,4 +85,32 @@ function includeFile($filename): bool
     }
 
     return true;
+}
+
+/**
+ * Get the evaluated view contents for the given view.
+ *
+ * @param  string  $view
+ * @param  array   $data
+ * @param  array   $mergeData
+ * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+ *
+ * @author Seven Du <shiweidu@outlook.com>
+ * @homepage http://medz.cn
+ */
+function view($view = null, $data = [], $mergeData = [])
+{
+    $finder = app(\Illuminate\View\FileViewFinder::class, [
+        'files' => app(\Illuminate\Filesystem\Filesystem::class),
+        'paths' => [base_path('views')],
+    ]);
+
+    $factory = app(\Illuminate\Contracts\View\Factory::class);
+    $factory->setFinder($finder);
+
+    if (func_num_args() === 0) {
+        return $factory;
+    }
+
+    return $factory->make($view, $data, $mergeData);
 }
