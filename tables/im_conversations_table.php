5ceb487f9cf9f1819c3384d2e58d1a5cec99a854
Seven <shiweidu@outlook.com>
2017-04-18 15:19:14 +0800

    Fixed table error.

--- tables/im_conversations_table.php
+++ tables/im_conversations_table.php
@@ -9,11 +9,11 @@ if (!Schema::hasTable('im_conversations', 'data')) {
         $table->increments('id')->comment('对话表表ID');
         $table->integer('user_id')->nullable()->default(0)->comment('创建对话用户UID');
         $table->bigInteger('cid')->index()->nullable()->default(0)->comment('对话id');
-        $table->string('name')->nullable()->dafault('')->comment('对话名称');
-        $table->string('pwd')->nullable()->default('')->comment('加入对话密码');
+        $table->string('name')->nullable()->dafault(null)->comment('对话名称');
+        $table->string('pwd')->nullable()->default(null)->comment('加入对话密码');
         $table->tinyInteger('is_disabled')->nullable()->default(0)->comment('是否被禁用,1:是 0:否');
         $table->tinyInteger('type')->nullable()->default(0)->comment('对话类型 0:私聊 1:群聊 2:聊天室');
-        $table->text('uids')->nullable()->default('')->comment('已加入聊天的用户UID');
+        $table->text('uids')->nullable()->comment('已加入聊天的用户UID');
         $table->timestamps();
     });
 }
