348cb30ec6b012c42a39c5803ea019b9b7e451f2
Seven <shiweidu@outlook.com>
2017-04-19 14:14:53 +0800

    更新help提示文案

--- views/admin.blade.php
+++ views/admin.blade.php
@@ -12,7 +12,7 @@
         {{ csrf_field() }}
         {{ method_field('PATCH') }}
         <input type="text" name="server" placeholder="请输入聊天服务器地址" value="{{ old('server', $server) }}" />
-        <p class="help-block">输入聊天服务器的连接地址，例如默认的「127.0.0.1:9900」。输入的服务器不存在或者错误，将会造成 app 等服务端的运行异常。</p>
+        <p class="help-block">输入聊天服务器的连接地址，例如默认的「127.0.0.1:9900」。输入的服务器不存在或者错误，将会造成 app 等客户端的运行异常。</p>
         <button>提交</button>
         @if (session('message'))
             <div class="alert alert-success">
