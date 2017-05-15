881edd72fe67c73df90ff4b97c32ce1fd38380eb
Seven <shiweidu@outlook.com>
2017-05-15 11:54:52 +0800

    增加公用导航。

--- views/admin.blade.php
+++ views/admin.blade.php
@@ -2,26 +2,30 @@
 
 @section('title', '服务地址设置')
 
-@section('head')
-<style type="text/css">@include('admin.style')</style>
-@endsection
+
+@push('heads')
+    <style type="text/css">@include('admin.style')</style>
+@endpush
 
 @section('content')
-<form role="form" method="POST" action="{{ route('im.manage.request') }}">
-    {{ csrf_field() }}
-    {{ method_field('PATCH') }}
-    <input type="text" name="server" placeholder="请输入聊天服务器地址" value="{{ old('server', $server) }}" />
-    <p class="help-block">输入聊天服务器的连接地址，例如默认的「127.0.0.1:9900」。输入的服务器不存在或者错误，将会造成 app 等客户端的运行异常。</p>
-    <button>提交</button>
-    @if (session('message'))
-        <div class="alert alert-success">
-            {{ session('message') }}
-        </div>
-    @endif
-    @if ($errors->has('server'))
-        <div class="alert alert-danger">
-            {{ $errors->first('server') }}
-        </div>
-    @endif
-</form>
+
+    @include('layouts.header')
+
+    <form role="form" method="POST" action="{{ route('im.manage.request') }}">
+        {{ csrf_field() }}
+        {{ method_field('PATCH') }}
+        <input type="text" name="server" placeholder="请输入聊天服务器地址" value="{{ old('server', $server) }}" />
+        <p class="help-block">输入聊天服务器的连接地址，例如默认的「127.0.0.1:9900」。输入的服务器不存在或者错误，将会造成 app 等客户端的运行异常。</p>
+        <button>提交</button>
+        @if (session('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+        @endif
+        @if ($errors->has('server'))
+            <div class="alert alert-danger">
+                {{ $errors->first('server') }}
+            </div>
+        @endif
+    </form>
 @endsection
--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -6,13 +6,16 @@
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>「ThinkSNS+」 - @yield('title')</title>
-    <link rel="stylesheet" type="text/css" href="{{ Zhiyi\Component\ZhiyiPlus\PlusComponentIm\asset('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ Zhiyi\Component\ZhiyiPlus\PlusComponentIm\asset('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ Zhiyi\Component\ZhiyiPlus\PlusComponentIm\asset('app.css') }}">
     <script src="{{ Zhiyi\Component\ZhiyiPlus\PlusComponentIm\asset('jquery-3.2.1.min.js') }}"></script>
-    @yield('head')
+    @stack('heads')
 </head>
 <body>
-@yield('content')
+<div class="container">
+    @yield('content')
+</div>
 <script src="{{ Zhiyi\Component\ZhiyiPlus\PlusComponentIm\asset('bootstrap/js/bootstrap.min.js') }}"></script>
-@yield('foot')
+@stack('foots')
 </body>
 </html>
