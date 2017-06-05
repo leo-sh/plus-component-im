1f0ed496d95e2b4866d7e13c4b74eba700c24de6
Seven <shiweidu@outlook.com>
2017-06-05 17:36:12 +0800

    发布各项资源

--- src/ImServiceProvider.php
+++ src/ImServiceProvider.php
@@ -6,13 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 class ImServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        // TODO.
-    }
-
     public function boot()
     {
-        // TODO.
+        // Load routes.
+        $this->loadRoutesFrom(
+            dirname(__DIR__).'/routes.php'
+        );
+
+        // Load views.
+        $this->loadViewsFrom(dirname(__DIR__).'/views/', 'component-im');
+
+        // publish asstes
+        $this->publishes([
+            dirname(__DIR__).'/asstes' => $this->app->publicPath().'/zhiyi/im'
+        ], 'public');
     }
 }
