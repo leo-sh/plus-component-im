d2afe2228734eb5eb3985f42bbc35793f74cb91d
Seven <shiweidu@outlook.com>
2017-06-05 16:32:14 +0800

    架构文件改为 ThinkSNS+ 最新拓展包模式。

---
+++ src/ImServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zhiyi\Component\ZhiyiPlus\PlusComponentIm;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // TODO.
+    }
+
+    public function boot()
+    {
+        // TODO.
+    }
+}
