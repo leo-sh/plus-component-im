b27403566f7316d0ce2de35c753d11d5799e38e3
白石 <414606094@qq.com>
2017-09-07 10:59:18 +0800

    修改权限删除错误

--- database/down.php
+++ database/down.php
@@ -1,11 +1,11 @@
 <?php
 
-use Zhiyi\Plus\Models\Permission;
+use Zhiyi\Plus\Models\Ability;
 use Zhiyi\Plus\Models\CommonConfig;
 use Illuminate\Support\Facades\Schema;
 
 // delete perm.
-Permission::where('name', 'like', 'im-%')->delete();
+Ability::where('name', 'like', 'im-%')->delete();
 
 // delete config.
 CommonConfig::byNamespace('common')->where('name', 'like', 'im:%')->delete();
