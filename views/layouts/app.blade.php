9cb65336f95c24df0164877b29c0c61867332fe0
boxshadow <idafoo@sina.com>
2017-10-31 20:26:00 +0800

    静态资源 没改完?

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>「ThinkSNS+」 - @yield('title')</title>
     <link rel="stylesheet" href="{{ asset('assets/im/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('zhiyi/im/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/im/app.css') }}">
     <script src="{{ asset('assets/im/jquery-3.2.1.min.js') }}"></script>
     @stack('heads')
 </head>
